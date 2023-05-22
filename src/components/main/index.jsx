2ea012ff858e41f259d11c53dc9f2bf2988e8f92
hwangjinseong <imhjs2023@gmail.com>
2023-05-22 11:45:22 +0900

    file setting

---
+++ src/components/main/index.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Main = () => {
+  return (
+    <MainContainer>
+      
+    </MainContainer>
+  )
+}
+
+export default Main;
+
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+const theme = {
+  color: {
+    black: "#000000",
+    white: "#FFFFFF",
+    primary: "#006EFF",
+    check: "#04DF00",
+    error: "#DC3035",
+    gray01: "#EDEDED",
+    graymain: "#D3D3D3",
+    gray02: "#A7A7A7",
+    gray03: "#505050",
+  },
+  font: {
+    noto: "Noto Sans",
+  },
+};
+
+export default theme;
